1bfd518ac07aea86179d377b5c8819edf4e86ed3
Cody McNeill <codymcneill@live.com>
2023-03-06 17:19:50 -0700

    Initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css"
+
+const Hero = () =>{
+    return (
+        <div className="Hero">
+            <h1>Welcome to the Concert Playlist Generator!</h1>
+            <p>Create a Spotify Playlist with the artists of your future concerts!</p>
+        </div>
+    )
+};
+
+export default Hero;
